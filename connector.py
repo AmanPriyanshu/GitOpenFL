5a4933a0d05ea1ff180357b14356493388e3ffe3
Aman Priyanshu <aman.priyanshu@learner.manipal.edu>
2020-12-16 15:37:50 +0530

    building a simple pytorch model

--- connector.py
+++ connector.py
@@ -23,11 +23,15 @@ class ClientConnector:
 	def check_existence(self):
 		links = ['https://github.com/'+client['username']+'/GitOpenFL/tree/'+client['branch'] for client in self.client_ids]
 		responses = [requests.get(link).status_code for link in links]
-		self.client_ids = [{'username' : client['username'], 'branch' : client['branch'], 'existence' : (response == 200)} for client, response in zip(self.client_ids, responses)]
+		self.client_ids = [{'client_update':'https://raw.githubusercontent.com/'+client['username']+'/GitOpenFL/'+client['branch']+'/current_epoch/update.pth', 'history_link' : 'https://raw.githubusercontent.com/'+client['username']+'/GitOpenFL/'+client['branch']+'/history/history.csv', 'username' : client['username'], 'branch' : client['branch'], 'existence' : (response == 200)} for client, response in zip(self.client_ids, responses)]
 		print("number of clients verified : "+str(sum([1 if client['existence'] else 0 for client in self.client_ids])))
 		print("\n--- users which could not be verified ---")
 		[print('{username: "'+str(client['username'])+'", branch: "'+str(client['branch'])+'"}') for client in self.client_ids if client['existence']==False]
-		
+	
+	def history_check(self, current_epoch=0):
+		pass
 
 if __name__ == '__main__':
 	c = ClientConnector()
+	print(c.client_ids)
+	#c.history_check()
